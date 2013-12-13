e409bae82773312b2fb94849da3eec2822a55ead
Joel Tong <me@joeltong.org>
2013-12-13 18:13:41 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kuler2gplWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
